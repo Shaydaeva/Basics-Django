c1f2d3c388a05d55a8199e8cf4a140fc11432e20
Shaydaeva <heartless416@gmail.com>
2020-12-21 03:05:58 +0300

    hw_05

--- authapp/views.py
+++ authapp/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.contrib import auth
+from django.contrib import auth, messages
 from django.urls import reverse
-from authapp.forms import UserLoginForm, UserRegisterForm
+from authapp.forms import UserLoginForm, UserRegisterForm, UserProfileForm
+from basketapp.models import Basket
 
 
 def login(request):
+    title = 'Вход'
+
     if request.method == 'POST':
         form = UserLoginForm(data=request.POST)
         if form.is_valid():
-    # if request.method == 'POST' and form.is_valid():
             username = request.POST['username']
             password = request.POST['password']
 
@@ -19,23 +21,46 @@ def login(request):
     else:
         form = UserLoginForm()
 
-    context = {'form': form}
+    context = {'title': title, 'form': form}
 
     return render(request, 'authapp/login.html', context)
 
 
 def register(request):
+    title = 'Регистрация'
+
     if request.method == 'POST':
         form = UserRegisterForm(data=request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('authapp:login'))
+            messages.success(request, 'Вы успешно зарегистрировались!')
+            return HttpResponseRedirect(reverse('auth:login'))
     else:
         form = UserRegisterForm()
 
-    context = {'form': form}
+    context = {'title': title, 'form': form}
 
     return render(request, 'authapp/register.html', context)
+
+
+def profile(request):
+    title = 'Мой профиль'
+
+    if request.method == 'POST':
+        form = UserProfileForm(data=request.POST, files=request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('auth:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+
+    context = {
+        'title': title,
+        'form': form,
+        'baskets': Basket.objects.filter(user=request.user),
+    }
+
+    return render(request, 'authapp/profile.html', context)
 
 
 def logout(request):
