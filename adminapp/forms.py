b1c47ebb785076679c29214c8c0dcd67d7231278
Shaydaeva <heartless416@gmail.com>
2020-12-26 22:31:41 +0300

    hw_07

--- adminapp/forms.py
+++ adminapp/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 from authapp.forms import UserRegisterForm, UserProfileForm
 from authapp.models import User
+from mainapp.models import ProductCategory
 
 
 class UserAdminRegisterForm(UserRegisterForm):
@@ -22,3 +23,16 @@ class UserAdminProfileForm(UserProfileForm):
         super(UserAdminProfileForm, self).__init__(*args, **kwargs)
         self.fields['username'].widget.attrs['readonly'] = False
         self.fields['email'].widget.attrs['readonly'] = False
+
+
+class AdminProductCategory(forms.ModelForm):
+
+    class Meta:
+        model = ProductCategory
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(AdminProductCategory, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+            field.help_text = ''
--- adminapp/views.py
+++ adminapp/views.py
@@ -3,7 +3,8 @@ from django.urls import reverse
 from django.contrib.auth.decorators import user_passes_test
 
 from authapp.models import User
-from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm
+from adminapp.forms import UserAdminRegisterForm, UserAdminProfileForm, AdminProductCategory
+from mainapp.models import ProductCategory
 
 
 @user_passes_test(lambda u: u.is_superuser)
@@ -56,3 +57,48 @@ def admin_users_delete(request, user_id):
     user.is_active = False
     user.save()
     return HttpResponseRedirect(reverse('admin_staff:admin_users'))
+
+
+def admin_categories(request):
+    context = {
+        'categories': ProductCategory.objects.all(),
+    }
+    return render(request, 'adminapp/admin-categories-read.html', context)
+
+
+def admin_categories_create(request):
+    if request.method == 'POST':
+        form = AdminProductCategory(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admin_staff:admin_categories'))
+    else:
+        form = AdminProductCategory()
+
+    context = {'form': form}
+
+    return render(request, 'adminapp/admin-categories-create.html', context)
+
+
+def admin_categories_update(request, cat_id):
+    category = ProductCategory.objects.get(id=cat_id)
+    if request.method == 'POST':
+        form = AdminProductCategory(data=request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('admin_staff:admin_categories'))
+    else:
+        form = AdminProductCategory(instance=category)
+
+    context = {'form': form, 'category': category}
+
+    return render(request, 'adminapp/admin-categories-update-delete.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def admin_categories_delete(request, cat_id):
+    category = ProductCategory.objects.get(id=cat_id)
+    category.is_active = False
+    category.save()
+    return HttpResponseRedirect(reverse('admin_staff:admin_categories'))
+
